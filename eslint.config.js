d43e6a7d34eb1c16e0ca0a418944a8a6e6e98d4a
Chaewon <codenamewont@gmail.com>
2026-02-11 03:48:26 +0900

    chore: 코드 포맷 및 린트 자동화 환경 구축

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,22 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
 
 module.exports = defineConfig([
   expoConfig,
+  require('eslint-config-prettier'),
+
   {
     ignores: ['dist/*'],
+
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
   },
 ]);
